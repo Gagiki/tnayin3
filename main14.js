8bb730d775c9ebac438af1a30b84cd6dc57221bd
Levon <levon90virabyan@gmail.com>
2023-02-11 22:04:49 +0400

    aster

---
+++ main14.js
@@ -0,0 +1,89 @@
+let country = ("Armenia");
+let country_1=country .charAt (2);
+console.log(country_1);
+// 
+let country_2 = ("Armenia");
+let country_3=country_2 .concat ("sity", "Erevan");
+console.log(country_3);
+// 
+let country_4 = ("Armenia");
+let country_5=country_4 .includes ("Arm");
+console.log(country_5);
+// 
+let country_6 = ("Armenia");
+let country_7=country_6 .indexOf ("a");
+console.log(country_7);
+// 
+let country_8 = ("Armenia");
+let country_9=country_8 .replace("lhg");
+console.log(country_9);
+// 
+let country_10 = ("Armenia");
+let country_11=country_10 .split("m");
+console.log(country_11);
+// 
+let country_12 = ("Armenia");
+let country_13=country_12 .startsWith("A");
+console.log(country_13);
+// 
+let country_14 = ("ARMENIA");
+let country_15=country_14 .toLowerCase();
+console.log(country_15);
+// 
+let country_16 = ("Armenia");
+let country_17=country_16.toUpperCase();
+console.log(country_17);
+// 
+let country_18 = ("ARMENIA");
+let country_19=country_18 .trim ("ARM  a"); 
+console.log(country_19);
+    // 
+let a =("12.3");
+let b =Number(); 
+console.log(Number());
+// 
+let x =("1213");
+let y = parseFloat(); 
+console.log (Number.parseFloat());
+// 
+let country_20 = ("ARMENIA");
+let country_21=country_20 .concat ("ARM  a"); 
+console.log(country_21);
+// 
+let users = [
+    {
+        name: "Anna",
+        age: "23",
+    },
+    {
+        name: "Gevorg",
+        age: "28",
+    },
+    {
+        name: "Vahe",
+        age: "40",
+    },
+] 
+users .filter((el,index,list) =>{
+    
+})
+console.log(users);
+// 
+let people = [
+    {
+        name: "Anna",
+        age: "23",
+    },
+    {
+        name: "Gevorg",
+        age: "28",
+    },
+    {
+        name: "Vahe",
+        age: "40",
+    },
+] 
+people .map((el) =>{
+    
+})
+console.log(people);
